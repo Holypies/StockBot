a02da005fb79789a9c73ba6969ebd87f7f793e3e
Felix Mårtensson <felix.martensson@telia.com>
2026-03-13 16:43:19 +0100

    added a simple moving average filter to be able to estimate stock statistical mean without worring about variance to much

---
+++ stockbot/moving_average_filter.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class MovingAverageFilter:
+    def __init__(self, window_size, **kwargs):
+        self.window = []
+        self.window_size = window_size
+        self.weigths = kwargs.get('weigths', np.ones(window_size))
+
+    def insert_new_value(self, u):
+        if len(self.window) <= self.window_size:
+            self.window.append(u)
+        else:
+            self.window.pop()
+            self.window.append(u)
+    
+    def get_average(self):
+        return np.sum(self.window)/len(self.window)
+    
+    def get_weigthed_average(self):
+        return np.convolve(self.window,self.weigths)
+    
+
+# Simple tests
+#measurements = [3,5,6,1,2,2,5,7,9,2,3,5,7,2,1,4,3]
+#
+#MA = MovingAverageFilter(5)
+#s = 0 
+#r = 0
+#for i in measurements:
+#    MA.insert_new_value(i)
+#    print(MA.get_average())
+#    s = i+s
+#    r+=1;
+#    a = s/r;
+#    print("true avg: ", a)
